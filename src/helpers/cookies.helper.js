a051807a12f72f42585bcfd5daf67799c3636d2d
FlorianLz <46797434+FlorianLz@users.noreply.github.com>
2022-02-28 19:09:24 +0100

    Ajout consentement pour l'affichage de la map

--- src/helpers/cookies.helper.js
+++ src/helpers/cookies.helper.js
@@ -6,13 +6,13 @@ export const cookiesHelper = {
         let val = JSON.stringify(cvalue);
         return document.cookie = cname + "=" + val + ";expires=" + expires + ";path=/";
     },
-    getCookiesFavs(cname){
-        const name = cname + "=";
-        const cDecoded = decodeURIComponent(document.cookie);
-        const cArr = cDecoded.split(name);
-        if(cArr[1]){
-            let res = JSON.parse(cArr[1]);
-            return res;
+    getCookiesFavs(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return JSON.parse(c.substring(nameEQ.length,c.length));
         }
         return [];
     },
@@ -20,5 +20,24 @@ export const cookiesHelper = {
         let expiration = new Date();
         expiration.setMonth(expiration.getMonth() + 12)
         return expiration;
+    },
+    getConsentValue() {
+        const consent = cookiesHelper.getCookie('CookieConsent');
+        if(consent) {
+            return consent;
+        }
+        return false;
+    },
+    getCookie(cname){
+        const name = cname + "=";
+        const cDecoded = decodeURIComponent(document.cookie);
+        const cArr = cDecoded.split(name);
+        if(cArr[1]){
+            return cArr[1].split(";")[0];
+        }
+        return false;
+    },
+    deleteCookie(cname) {
+        document.cookie = cname + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     }
 }
